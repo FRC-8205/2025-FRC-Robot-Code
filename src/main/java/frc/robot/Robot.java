e16509b953b21bb1fe58c9c0f3a13b7c3c8bd016
Ray <ray.mcdonald25@capefearacademy.org>
2025-03-28 23:55:19 -0400

    Max Fix?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -37,7 +37,14 @@ public class Robot extends TimedRobot {
   public Robot() {
     m_robotContainer = new RobotContainer();
 
-    m_robotContainer.drivetrain.resetPose(new Pose2d(5.6, 5.8, new Rotation2d(-120)));
+    var visionEst = m_robotContainer.vision.getEstimatedGlobalPose();
+    visionEst.ifPresent(est -> {
+        var estStdDevs = m_robotContainer.vision.getEstimationStdDevs();
+        m_robotContainer.drivetrain.addVisionMeasurement(
+            est.estimatedPose.toPose2d(), est.timestampSeconds, estStdDevs
+        );
+        m_robotContainer.drivetrain.resetPose(est.estimatedPose.toPose2d());
+    });
 
     PDH = new PowerDistribution(1, ModuleType.kRev);
 
